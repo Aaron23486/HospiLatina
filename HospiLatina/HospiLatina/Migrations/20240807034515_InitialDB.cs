7cef3c9dd00992833fdb2fdeb94ab4faf9dff83b
Aaron234786 <aaron20770@gmail.com>
2024-08-14 18:36:20 -0600

    SaveRamaVieja

---
+++ HospiLatina/HospiLatina/Migrations/20240807034515_InitialDB.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospiLatina.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consultorios",
+                columns: table => new
+                {
+                    IdConsultorio = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ubicacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultorios", x => x.IdConsultorio);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipos",
+                columns: table => new
+                {
+                    IdEquipo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaSoporte = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipos", x => x.IdEquipo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiantes",
+                columns: table => new
+                {
+                    IdEstudiante = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Especialidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnoDeGraduacion = table.Column<int>(type: "int", nullable: false),
+                    HorarioAtencion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiantes", x => x.IdEstudiante);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    IdPaciente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.IdPaciente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Procedimientos",
+                columns: table => new
+                {
+                    IdProcedimiento = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Procedimientos", x => x.IdProcedimiento);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profesores",
+                columns: table => new
+                {
+                    IdProfesor = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Especialidad = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profesores", x => x.IdProfesor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalasCirugia",
+                columns: table => new
+                {
+                    IdSala = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ubicacion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalasCirugia", x => x.IdSala);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Facturas",
+                columns: table => new
+                {
+                    IdFactura = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPaciente = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Total = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facturas", x => x.IdFactura);
+                    table.ForeignKey(
+                        name: "FK_Facturas_Pacientes_IdPaciente",
+                        column: x => x.IdPaciente,
+                        principalTable: "Pacientes",
+                        principalColumn: "IdPaciente",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Citas",
+                columns: table => new
+                {
+                    IdCita = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdProfesor = table.Column<int>(type: "int", nullable: false),
+                    IdProcedimiento = table.Column<int>(type: "int", nullable: false),
+                    IdEstudiante = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Hora = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdConsultorio = table.Column<int>(type: "int", nullable: false),
+                    IdPaciente = table.Column<int>(type: "int", nullable: false),
+                    IdSala = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Citas", x => x.IdCita);
+                    table.ForeignKey(
+                        name: "FK_Citas_Consultorios_IdConsultorio",
+                        column: x => x.IdConsultorio,
+                        principalTable: "Consultorios",
+                        principalColumn: "IdConsultorio",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_Estudiantes_IdEstudiante",
+                        column: x => x.IdEstudiante,
+                        principalTable: "Estudiantes",
+                        principalColumn: "IdEstudiante",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_Pacientes_IdPaciente",
+                        column: x => x.IdPaciente,
+                        principalTable: "Pacientes",
+                        principalColumn: "IdPaciente",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_Procedimientos_IdProcedimiento",
+                        column: x => x.IdProcedimiento,
+                        principalTable: "Procedimientos",
+                        principalColumn: "IdProcedimiento",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_Profesores_IdProfesor",
+                        column: x => x.IdProfesor,
+                        principalTable: "Profesores",
+                        principalColumn: "IdProfesor",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_SalasCirugia_IdSala",
+                        column: x => x.IdSala,
+                        principalTable: "SalasCirugia",
+                        principalColumn: "IdSala",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetallesFactura",
+                columns: table => new
+                {
+                    IdDetalle = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdFactura = table.Column<int>(type: "int", nullable: false),
+                    IdProcedimiento = table.Column<int>(type: "int", nullable: false),
+                    Subtotal = table.Column<double>(type: "float", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetallesFactura", x => x.IdDetalle);
+                    table.ForeignKey(
+                        name: "FK_DetallesFactura_Facturas_IdFactura",
+                        column: x => x.IdFactura,
+                        principalTable: "Facturas",
+                        principalColumn: "IdFactura",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetallesFactura_Procedimientos_IdProcedimiento",
+                        column: x => x.IdProcedimiento,
+                        principalTable: "Procedimientos",
+                        principalColumn: "IdProcedimiento",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Calificaciones",
+                columns: table => new
+                {
+                    IdCalificacion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Puntuacion = table.Column<int>(type: "int", nullable: false),
+                    Comentario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdCita = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calificaciones", x => x.IdCalificacion);
+                    table.ForeignKey(
+                        name: "FK_Calificaciones_Citas_IdCita",
+                        column: x => x.IdCita,
+                        principalTable: "Citas",
+                        principalColumn: "IdCita",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calificaciones_IdCita",
+                table: "Calificaciones",
+                column: "IdCita");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_IdConsultorio",
+                table: "Citas",
+                column: "IdConsultorio");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_IdEstudiante",
+                table: "Citas",
+                column: "IdEstudiante");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_IdPaciente",
+                table: "Citas",
+                column: "IdPaciente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_IdProcedimiento",
+                table: "Citas",
+                column: "IdProcedimiento");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_IdProfesor",
+                table: "Citas",
+                column: "IdProfesor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_IdSala",
+                table: "Citas",
+                column: "IdSala");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesFactura_IdFactura",
+                table: "DetallesFactura",
+                column: "IdFactura");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesFactura_IdProcedimiento",
+                table: "DetallesFactura",
+                column: "IdProcedimiento");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Facturas_IdPaciente",
+                table: "Facturas",
+                column: "IdPaciente");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calificaciones");
+
+            migrationBuilder.DropTable(
+                name: "DetallesFactura");
+
+            migrationBuilder.DropTable(
+                name: "Equipos");
+
+            migrationBuilder.DropTable(
+                name: "Citas");
+
+            migrationBuilder.DropTable(
+                name: "Facturas");
+
+            migrationBuilder.DropTable(
+                name: "Consultorios");
+
+            migrationBuilder.DropTable(
+                name: "Estudiantes");
+
+            migrationBuilder.DropTable(
+                name: "Procedimientos");
+
+            migrationBuilder.DropTable(
+                name: "Profesores");
+
+            migrationBuilder.DropTable(
+                name: "SalasCirugia");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
